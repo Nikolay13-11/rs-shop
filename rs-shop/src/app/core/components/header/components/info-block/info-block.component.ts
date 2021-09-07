8875cdfcb9d593a61b12760936953964ccf29dae
Nikolay13-11 <mikola13.11@mail.ru>
2021-09-08 02:08:35 +0300

    add header elements

--- rs-shop/src/app/core/components/header/components/info-block/info-block.component.ts
+++ rs-shop/src/app/core/components/header/components/info-block/info-block.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-info-block',
   templateUrl: './info-block.component.html',
   styleUrls: ['./info-block.component.scss']
 })
-export class InfoBlockComponent implements OnInit {
+export class InfoBlockComponent {
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
