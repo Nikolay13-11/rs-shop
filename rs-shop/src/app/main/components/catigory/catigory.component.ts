a6c8c3610da77e9d0e23e3ae425e7d4a6bec3c4c
Nikolay13-11 <mikola13.11@mail.ru>
2021-09-23 02:18:32 +0300

    fix styles

--- rs-shop/src/app/main/components/catigory/catigory.component.ts
+++ rs-shop/src/app/main/components/catigory/catigory.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { Observable } from 'rxjs';
+
 import { DataFromHttpService } from '../../services/data-from-http.service';
 
 @Component({
@@ -23,8 +25,10 @@ export class CatigoryComponent implements OnInit{
   updateCategory() {
     const  { categoriesId, categoryId }  = this.activeRoute.snapshot.params;
     this.subCategory = categoryId;
+
     this.dataService.nextGoodsByCategory(categoriesId, categoryId)
     this.goodsByCatigory$ = this.dataService.sharedGoodsByCategory
+    console.log(this.dataService.sharedSubCategories)
     // this.categoryById$ = this.dataService.sharedCategoryById
     // this.subCategoryById$ = this.dataService.sharedSubCategoryById
   }
